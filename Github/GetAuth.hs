d83e454271d2d984a7932832cbcdbe08074c50b8
Joey Hess <joeyh@joeyh.name>
2020-07-21 23:23:57 -0400

    Removed support for GITHUB_USER/GITHUB_PASSWORD
    Since github will soon remove support for http basic auth. Use
    GITHUB_OAUTH_TOKEN instead.
    
    This commit was sponsored by Svenne Krap on Patreon.

--- Github/GetAuth.hs
+++ Github/GetAuth.hs
@@ -1,27 +1,30 @@
 module Github.GetAuth where
 
 import Utility.Env
-import Utility.Monad
+import Data.Maybe
+import System.IO
 
+import Common
 import qualified GitHub.Auth as Github
-import Data.Text.Encoding (encodeUtf8)
-import qualified Data.Text as T
 import qualified Data.ByteString.UTF8 as B
 
 getAuth :: IO (Maybe Github.Auth)
-getAuth = getM id
-	[ do
-		user <- getEnv "GITHUB_USER"
-		password <- getEnv "GITHUB_PASSWORD"
-		return $ case (user, password) of
-			(Just u, Just p) -> Just $ 
-				Github.BasicAuth (tobs u) (tobs p)
-			_ -> Nothing
-	, do
-		oauthtoken <- getEnv "GITHUB_OAUTH_TOKEN"
-		return $ case oauthtoken of
-			Just t -> Just $ Github.OAuth (B.fromString t)
-			Nothing -> Nothing
-	]
-  where
-	tobs = encodeUtf8 . T.pack
+getAuth = do
+	oauthtoken <- getEnv "GITHUB_OAUTH_TOKEN"
+	case oauthtoken of
+		Just t -> return $ Just $ Github.OAuth (B.fromString t)
+		Nothing -> do
+			checkDeprecatedVars
+			return Nothing
+
+checkDeprecatedVars :: IO ()
+checkDeprecatedVars = do
+	user <- getEnv "GITHUB_USER"
+	password <- getEnv "GITHUB_PASSWORD"
+	when (isJust user && isJust password) $
+		hPutStrLn stderr $ unwords
+			[ "GITHUB_USER and GITHUB_PASSWORD are no longer"
+			, "supported, because Github is removing that"
+			, "authentication method."
+			, "Set GITHUB_OAUTH_TOKEN instead."
+			]
